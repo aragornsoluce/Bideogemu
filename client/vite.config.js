46199ba83c75f223ca6b3ad7c2adec4a9efdb57f
Paul Teillet <125264051+aragornsoluce@users.noreply.github.com>
2025-04-01 18:25:12 +0200

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src', // Définit @ comme alias vers le dossier src
+    },
+  },
+});
